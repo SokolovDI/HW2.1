f940ec18e57b23e21dfb0809565306c71a385eb7
Dmitrii <sokolov.di@inbox.ru>
2021-03-17 22:28:53 +0300

    hw 2.2 is done

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -10,4 +10,64 @@
  */
 
 public class Exceptions {
+    public static void main(String[] args) {
+
+        String[][] array = new String[][]{
+                {"1", "1", "1", "1"},
+                {"2", "2", "2", "2"},
+                {"3", "3", "3", "3"},
+                {"4", "4", "4", "4"}};
+
+//        String[][] array = new String[][]{
+//                {"1", "1", "1", "1"},
+//                {"1", "1", "1", "1"},
+//                {"2", "2", "2", "2"},
+//                {"3", "3", "3", "3"},
+//                {"4", "4", "4", "4"}};
+
+//        String[][] array = new String[][]{
+//                {"1", "1", "1", "1", "1"},
+//                {"2", "2", "2", "2"},
+//                {"3", "3", "3", "3"},
+//                {"4", "4", "4", "4"}};
+
+//        String[][] array = new String[][]{
+//                {"a", "1", "1", "1"},
+//                {"2", "2", "2", "2"},
+//                {"3", "3", "3", "3"},
+//                {"4", "4", "4", "4"}};
+
+        try {
+            try {
+                int res = run(array);
+                System.out.println(res);
+            } catch (MyArraySizeException ignored) {
+            }
+        } catch (MyArrayDataException e) {
+            System.out.println("Проверь координату: " + e.a + " x " + e.b);
+        }
+
+    }
+
+    private static int run(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException();
+        }
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum = sum + Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return sum;
+    }
 }
+
---
+++ src/MyArraySizeException.java
@@ -0,0 +1,5 @@
+public class MyArraySizeException extends Exception{
+    public MyArraySizeException() {
+        System.out.println("Введите корректный размер массива");
+    }
+}
